aafd3bc352b67799d94859853958c4501a2ee7d3
DESKTOP-JRC8315\79527 <raketa.souz@gmail.com>
2021-02-18 15:21:54 +0300

    element

--- src/App.js
+++ src/App.js
@@ -1,32 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-import {count, length} from './number'
+import React from 'react';
 
+const element = (<div>
+  Hello world!
+</div>);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-
-        <p>
-          count * length = {count*length}
-        </p>
-        
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export {element}
--- src/number.js
+++
@@ -1,4 +0,0 @@
-const count = 5;
-const length = 3;
-
-export { count, length}
