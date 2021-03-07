f5cd1260ec6e82989d426d1922712b6147151919
DESKTOP-JRC8315\79527 <raketa.souz@gmail.com>
2021-03-07 17:31:53 +0300

    stylization

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import styles from './Footer.module.css';
 
-const Footer = ({ count }) => (<div>
-    <div>Невыполненных заданий: {count} </div>
-    <ul>
+const Footer = ({ count }) => (<div className={styles.footer}>
+    <div className={styles.unfulfilled}>Невыполненно: {count} </div>
+    <ul className={styles.menu}>
         <li>Все</li>
-        <li>Выполненные</li>
-        <li>Невыполненные</li>
+        <li>Выполнено</li>
+        <li>Невыполненно</li>
     </ul>
     <div>Удалить выполненные</div>
 </div>);
--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import Item from '../Item/Item';
+import styles from './ItemList.module.css';
+import Checkbox from '@material-ui/core/Checkbox';
+import DeleteOutlinedIcon from '@material-ui/icons/DeleteOutlined';
 
-const ItemList = ({ items }) => (<ul>
-    {items.map( item => <li key={item.value}>
+const ItemList = ({ items }) => (<ul classsName={styles.item_list}>
+    {items.map( item => <li key={item.value} className={styles.item}>
+        <Checkbox
+        defaultChecked
+        color="primary"
+        inputProps={{ 'aria-label': 'secondary checkbox' }}
+      />
         <Item value={item.value} isDone={item.isDone} />
+        <DeleteOutlinedIcon className={styles.icon} />
         </li>)}
 </ul>);
 
