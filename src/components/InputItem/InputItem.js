7749502291c3e8f0920ca6dc608fe066bc6be187
DESKTOP-JRC8315\79527 <raketa.souz@gmail.com>
2021-03-07 09:04:21 +0300

    UI

--- src/components/InputItem/InputItem.js
+++ src/components/InputItem/InputItem.js
@@ -1,5 +1,9 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
 
-const InputItem = () =>(<input></input>);
+const InputItem = () =>(
+    <div>
+        <TextField id="outlined-basic" label="Добавить задание" variant="outlined" />
+    </div>);
 
 export default InputItem;
