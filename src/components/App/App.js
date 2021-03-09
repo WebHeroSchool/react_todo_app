5c0ade01df8d7da3149cc6bbe0161a37fcd2fbfe
DESKTOP-JRC8315\79527 <raketa.souz@gmail.com>
2021-03-09 20:48:23 +0300

    state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,27 +9,31 @@ const props = {
 };
 
 class App extends React.Component {
+
+  state = {
+    items: [
+      {
+        value: 'Постричься',
+        isDone: true
+      },
+      {
+        value: 'Погладить вещи',
+        isDone: false
+      },
+      {
+        value: 'Сделать уборку дома',
+        isDone: true
+      }
+    ]
+  } 
+
   render () {
-      const items = [
-        {
-          value: 'Постричься',
-          isDone: true
-        },
-        {
-          value: 'Погладить вещи',
-          isDone: false
-        },
-        {
-          value: 'Сделать уборку дома',
-          isDone: true
-        }
-      ];
 
     return (
       <div className={styles.wrap}>
         <h1 className={styles.title}>Todos</h1>
         <InputItem />
-        <ItemList items={items}/>
+        <ItemList items={this.state.items}/>
         <Footer count={props.count} />
       </div>);
   }
